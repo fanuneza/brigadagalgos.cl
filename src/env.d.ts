28dbecfaf7369959d667ea75a3d6f0cf90d436e2
Fabián Núñez <fanuneza@gmail.com>
2026-06-29 21:07:37 -0400

    feat: enhance form attributes and improve type definitions
    - Added custom attributes to the ContactForm component for better tool integration and description.
    - Updated the StoriesSection component to simplify the dog mapping function.
    - Extended type definitions in env.d.ts to include new HTML attributes for enhanced type safety.

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="astro/client" />
+
+declare namespace astroHTML.JSX {
+  interface HTMLAttributes {
+    toolname?: string;
+    tooldescription?: string;
+    toolparamdescription?: string;
+  }
+}
